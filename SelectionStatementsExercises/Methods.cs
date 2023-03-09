9fee3c81c4a91d4766ba758f18105ed6732a3ff8
Seth Bowman <brandonsethbowman@gmail.com>
2023-03-09 10:07:30 -0600

    Add project files.

---
+++ SelectionStatementsExercises/Methods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SelectionStatementsExercises
+{
+    public class Methods
+    {
+        //Exercise 1
+        public static void GuessingGame()
+        {
+
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+            var response = 0;
+            while (response != favNumber)
+            {
+                Console.WriteLine("Guess a number between 1 and 10");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNumber)
+                {
+                    Console.WriteLine($"Too low\nYour Guess: {response}");
+                }
+                else if (response > favNumber)
+                {
+                    Console.WriteLine($"Too high\nYour Guess: {response}");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed correctly!\nThe number was {response}");
+                }
+                Thread.Sleep(3000);
+                Console.Clear();
+
+            }
+
+        }
+        public static void FavoriteSubject()
+        {
+            Console.WriteLine("What is your favorite subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I hate Math");
+                    break;
+                case "science":
+                    Console.WriteLine("I love Science");
+                    break;
+                case "pe":
+                    Console.WriteLine("PE is awesome");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my favorite");
+                    break;
+                default:
+                    Console.WriteLine("Oh.. nice.");
+                    break;
+            }
+        }
+    }
+}
---
+++ SelectionStatementsExercises/Program.cs
@@ -0,0 +1,8 @@
+﻿using SelectionStatementsExercises;
+using System.Threading.Channels;
+
+//Exercise 1
+Methods.GuessingGame();
+
+//Exercise 2
+Methods.FavoriteSubject();
