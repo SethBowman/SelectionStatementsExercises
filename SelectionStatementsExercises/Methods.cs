115a40c911461de7139c003416384a596bb6df88
Seth Bowman <brandonsethbowman@gmail.com>
2023-05-11 16:38:07 -0500

    Made code simpler for teaching example

--- SelectionStatementsExercises/Methods.cs
+++ SelectionStatementsExercises/Methods.cs
@@ -9,29 +9,45 @@ namespace SelectionStatementsExercises
         {
             var r = new Random();
             var favNumber = r.Next(1, 10);
-            var response = 0;
-            while (response != favNumber)
+            Console.WriteLine("Guess a number between 1 and 10");
+            var response = int.Parse(Console.ReadLine());
+
+            if (response < favNumber)
             {
-                var canParse = false;
-                Console.WriteLine("Guess a number between 1 and 10");
-                canParse = int.TryParse(Console.ReadLine(), out response);
-                while (canParse == false)
-                {
-                    Console.WriteLine("That was not a correct choice, try again.");
-                    Console.WriteLine("Guess a number between 1 and 10");
-                    canParse = int.TryParse(Console.ReadLine(), out response);
-                    Console.Clear();
-                }
-
-                var message = (response < favNumber) ? $"Too low\nYour Guess: {response}" :
-                    (response > favNumber) ? $"Too high\nYour Guess: {response}" :
-                    $"You guessed correctly!\nThe number was {response}";
-                Console.WriteLine(message);
-
-                Thread.Sleep(3000);
-                Console.Clear();
-
+                Console.WriteLine($"Too low\nYour Guess: {response}");
             }
+            else if (response > favNumber)
+            {
+                Console.WriteLine($"Too high\nYour Guess: {response}");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed correctly!\nThe number was {response}");
+            }
+
+            //var response = 0;
+            //while (response != favNumber)
+            //{
+            //    var canParse = false;
+            //    Console.WriteLine("Guess a number between 1 and 10");
+            //    canParse = int.TryParse(Console.ReadLine(), out response);
+            //    while (canParse == false)
+            //    {
+            //        Console.WriteLine("That was not a correct choice, try again.");
+            //        Console.WriteLine("Guess a number between 1 and 10");
+            //        canParse = int.TryParse(Console.ReadLine(), out response);
+            //        Console.Clear();
+            //    }
+
+                //    Console.WriteLine((response < favNumber) ? $"Too low\nYour Guess: {response}" :
+                //        (response > favNumber) ? $"Too high\nYour Guess: {response}" :
+                //        $"You guessed correctly!\nThe number was {response}");
+
+
+                //    Thread.Sleep(3000);
+                //    Console.Clear();
+
+                //}
 
         }
         public static void FavoriteSubject()
